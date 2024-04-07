d8b6eba78164927ad72367608fa5e1e77f130980
Myungjoo Lee <110808006+mjoo-lee@users.noreply.github.com>
2024-04-07 20:46:59 +0900

    Title: 포탑 부수기, Time: 146ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240407/포탑 부수기/destroy-the-turret.py
+++ 240407/포탑 부수기/destroy-the-turret.py
@@ -1,5 +1,6 @@
 from collections import deque 
 
+
 N,M,K = map(int, input().split())
 board = [[-1 for _ in range(N+1)]] + [[-1] + list(map(int, input().split())) for _ in range(N)]
 
@@ -7,8 +8,9 @@ def selectWeakest(board):
     port, time, power = [float('inf'),float('inf')], float('-inf'), float('inf')
     for n in range(1,N+1):
         for m in range(1,M+1):
-            if (power, -time, (port[0]+port[1]), -port[1], port) > (board[n][m], -recent[n][m], (n+m), -m, [n,m]) and board[n][m] > 0:
+            if (power, -time, -(port[0]+port[1]), -port[1]) > (board[n][m], -recent[n][m], -(n+m), -m) and board[n][m] > 0:
                 power, port, time = board[n][m], [n,m], recent[n][m] #업데이트
+        
     return port        
 
 def attack(board, n, m, k):
@@ -19,15 +21,16 @@ def attack(board, n, m, k):
     port, time, power = [float('-inf'),float('-inf')], float('inf'), float('-inf')
     for n in range(1,N+1):
         for m in range(1,M+1):
-            if (power, -time, -(port[0]+port[1]), -port[1], port) < (board[n][m], -recent[n][m], -(n+m), -m, [n,m]) and board[n][m] > 0:
+            if [n,m] != weakest and (power, -time, -(port[0]+port[1]), -port[1]) < (board[n][m], -recent[n][m], -(n+m), -m,) and board[n][m] > 0:
                 power, port, time = board[n][m], [n,m], recent[n][m] #업데이트
-        
+                #print("쎈거 고르는중:",port)
     
     #공격
     startR, startC = weakest[0], weakest[1]
     Power = board[startR][startC]
     halfPower = Power // 2
     #print("weakest:",startR, startC)
+    #print("strongest:",port[0],port[1])
     #이동방향 설정
     if abs(port[0]-startR) < abs(startR-port[0]): 
         dr = startR-port[0]
@@ -64,19 +67,16 @@ def attack(board, n, m, k):
     if laser:
         #print("레이저")
         #print("laser 경로:",path)
-        if path:
-            del path[0]
+        del path[0]
         targetR, targetC = port[0], port[1] 
         
         #중간 경로 공격
         while path:
             attackR, attackC = path.popleft()
             board[attackR][attackC] -= halfPower
-            recent[attackR][attackC] = k
 
         #최종 목표 공격
         board[targetR][targetC] -= Power
-        recent[targetR][targetC] = k
 
     #2.포탄 공격
     if not laser:
@@ -90,10 +90,14 @@ def attack(board, n, m, k):
             neighC = (targetC + dc) % (M+1)
             if neighR == 0:
                 neighR = 1
+            elif neighR < 0:
+                neighR += (N+1) 
             if neighC == 0:
                 neighC = 1
+            elif neighC < 0:
+                neighC += (C+1)
             
-            if board[neighR][neighC] != 0 and [neighR, neighC] not in neighbor: 
+            if [neighR, neighC] != port and board[neighR][neighC] != 0 and [neighR, neighC] not in neighbor: 
                 neighbor.append([neighR, neighC])
     
         #주변 공격
@@ -101,11 +105,9 @@ def attack(board, n, m, k):
         while neighbor:
             attackR, attackC = neighbor.popleft()
             board[attackR][attackC] -= halfPower
-            recent[attackR][attackC] = k
 
         #최종 목표 공격
         board[targetR][targetC] -= Power
-        recent[targetR][targetC] = k
         
     #포탑 부서짐
     for n in range(1,N+1):
@@ -113,7 +115,7 @@ def attack(board, n, m, k):
             if board[n][m] < 0:
                 board[n][m] = 0
 
-    return board
+    return board, targetR, targetC
 
 def countport(board):
     cnt = 0
@@ -135,17 +137,17 @@ for k in range(1,K+1):
     #1. 공격자 선정 -> 공격력 증가
     weakest = selectWeakest(board)
     board[weakest[0]][weakest[1]] += (N+M)
-    recent[weakest[0]][weakest[1]] = k
     #print("공격자 공격력 증가:", board)
     
     #2. 공격자의 공격 & 3. 포탑 부서짐 -> 판 업데이트 
-    board = attack(board, weakest[0], weakest[1], k)
+    board, targetR, targetC = attack(board, weakest[0], weakest[1], k)
     #print("공격:", board)
     
+    recent[weakest[0]][weakest[1]] = k
     #4. 포탑 정비 (부서지지 않은 포탑 중 공격과 무관한 탑 공격력 + 1)
     for n in range(1,N+1):
         for m in range(1,M+1):
-            if recent[n][m] < k and board[n][m] > 0:
+            if recent[n][m] < k and board[n][m] > 0 and [n,m] != [targetR, targetC]:
                 board[n][m] += 1
     #print("recent:",recent)
     #print(k,"번째 턴 이후:",board)
