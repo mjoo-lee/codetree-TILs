d970506f44cc586d1dee306f47659fda718526fd
Myungjoo Lee <110808006+mjoo-lee@users.noreply.github.com>
2024-04-11 16:33:51 +0900

    Title: 코드트리 빵, Time: 5053ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240411/코드트리 빵/codetree-mon-bread.py
+++ 240411/코드트리 빵/codetree-mon-bread.py
@@ -14,18 +14,35 @@ for i in range(1,n+1):
 canMove = [True for _ in range(m+1)]
 people = [[-1,-1] for _ in range(m+1)] #사람 위치 정보 
             
-def closest(targetR, targetC):
+from collections import deque
+
+def closest(targetR, targetC, t):
+    d = [(-1,0),(0,-1),(0,1),(1,0)] #상좌우하
+    
     
     minDist = float('inf')
     minR, minC = float('inf'), float('inf')
     for b in basecamp:
+        if b in prohibited:
+            continue
+            
         bR, bC = b
-        curDist = abs(bR-targetR) + abs(bC-targetC) - 1
-        if [bR, bC] not in prohibited:
-            if (minDist, minR, minC) > (curDist, bR, bC):
-                minDist = curDist
+        q = deque()
+        q.append((bR, bC, 0))
+        visited = set((bR, bC))
+        
+        while q:
+            curR, curC, cnt = q.popleft()
+            if curR == targetR and curC == targetC and (cnt, bR, bC) < (minDist, minR, minC):
+                minDist = cnt
                 minR, minC = bR, bC
-                
+                    
+            for dr, dc in d:
+                newR, newC = curR + dr, curC + dc
+                if 1<=newR<=n and 1<=newC<=n and [newR, newC] not in prohibited and (newR, newC) not in visited:
+                    q.append((newR, newC, cnt+1))
+                    visited.add((newR, newC))
+
     return minR, minC
 
 
@@ -49,7 +66,7 @@ def shortestPath(startR, startC, idx):
                     path = temp
             else:
                 path = temp
-        
+   
         for dr, dc in d:
             newR, newC = curR + dr, curC + dc
             #print("new", newR, newC)
@@ -80,13 +97,14 @@ def move(board, t):
     moved = False
     # 1. 베이스캠프로 이동
     if t <= m and not toBase[t]:
-        baseR, baseC = closest(targetR, targetC)
+        baseR, baseC = closest(targetR, targetC, t)
         #print(baseR, baseC)
         if [baseR, baseC] not in prohibited:
             people[t] = [baseR, baseC] #베이스캠프로 이동
             toBase[t] = True
             moved = True
-            prohibited.append([baseR, baseC]) 
+            if [baseR, baseC] not in prohibited:
+                prohibited.append([baseR, baseC]) 
             #print("베이스캠프 이동",people)
 
     # 2. 편의점 향해 1칸 이동
@@ -105,21 +123,27 @@ def move(board, t):
         return
 
 cnt = 0
-T = 0
 prohibited = []
 toBase = [False for _ in range(1+m)]
+
 while not allout(convi,people):
     cnt += 1
-    #print(cnt)
-    #print("prohibited",prohibited)
+#     print(cnt)
+#     print("prohibited",prohibited)
     if cnt <= m:
         for t in range(1,cnt+1):
-            #print(t)
-            #print("처음",people)
+#             print("t",t)
+#             print("처음",people)
             move(board, t)
     else:
         if not allout(convi,people):
             for t in range(1,m+1):
                 move(board,t)
+    
+    
+    
+#     print("convi",convi)
+#     print("people",people)
+#     print("================")
 
 print(cnt)
